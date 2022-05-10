9c34c934efd4a2612c8da1b65c13303efc8de005
ghufran adel <gho0ofran.adel14@gmail.com>
2022-05-10 06:13:26 -0700

    assignment is compeleted

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  const talk =  document.getElementById('text');
+talk.textContent ="This is really cool!"
+  });
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
